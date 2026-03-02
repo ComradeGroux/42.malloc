22bd7746cc1bb382aab3802365f4624fc0fd354f
Victor Groux <victorgroux99@gmail.com>
2026-03-02 11:54:59 +0100

    Free seems OK (tests was not executed)
    The function does coalescence to merge free neighbors into a single
    block

--- srcs/free.c
+++ srcs/free.c
@@ -2,8 +2,98 @@
 
 extern t_malloc_state	gMallocState;
 
+static t_heap	*check_part_of(t_heap *heap, t_block *block)
+{
+	while (heap)
+	{
+		char	*end = (char *)heap + heap->total_size;
+		if ((void *)block > (void *)heap && (void *)block < (void *)end)
+			return heap;
+		heap = heap->next;
+	}
+	return NULL;
+}
+
+static t_heap	*retrieve_heap(t_block *block)
+{
+	t_heap	*heap_head = NULL;
+
+	heap_head = check_part_of(gMallocState.tiny, block);
+	if (heap_head != NULL)
+		return heap_head;
+
+	heap_head = check_part_of(gMallocState.small, block);
+	if (heap_head != NULL)
+		return heap_head;
+
+	heap_head = check_part_of(gMallocState.large, block);
+	if (heap_head != NULL)
+		return heap_head;
+
+	return NULL;
+}
+
+static void	coalescence(t_heap *heap, t_block *block)
+{
+	if (block->next != NULL)
+	{
+		if (block->next->in_use == 0)
+		{
+			t_block	*next = block->next;
+			if (next->next != NULL)
+				next->next->prev = block;
+
+			block->next = next->next;
+			block->size += next->size + sizeof(t_block);
+
+			heap->block_count -= 1;
+			heap->free_size += sizeof(t_block);
+		}
+	}
+	if (block->prev != NULL)
+	{
+		if (block->prev->in_use == 0)
+		{
+			t_block	*prev = block->prev;
+			prev->next = block->next;
+			if (prev->next != NULL)
+				prev->next->prev = prev;
+			prev->size += block->size + sizeof(t_block);
+
+			heap->block_count -= 1;
+			heap->free_size += sizeof(t_block);
+		}
+	}
+}
+
 void	free(void *ptr)
 {
 	if (ptr == NULL)
 		return;
+
+	t_block	*block = (t_block *)((char *)ptr - sizeof(t_block));
+	t_heap	*heap = retrieve_heap(block);
+	if (heap == NULL)
+		return;
+
+	block->in_use = 0;
+	heap->free_size += block->size;
+	coalescence(heap, block);
+
+	if (heap->free_size == (heap->total_size - sizeof(t_heap) - sizeof(t_block)))
+	{
+		if (heap->prev != NULL)
+			heap->prev->next = heap->next;
+		if (heap->next != NULL)
+			heap->next->prev = heap->prev;
+
+		if (heap->group == TINY && gMallocState.tiny == heap)
+			gMallocState.tiny = heap->next;
+		else if (heap->group == SMALL && gMallocState.small == heap)
+			gMallocState.small = heap->next;
+		else if (heap->group == LARGE && gMallocState.large == heap)
+			gMallocState.large = heap->next;
+
+		munmap(heap, heap->total_size);
+	}
 }
