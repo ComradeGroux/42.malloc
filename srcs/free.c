ac98c1d6d6f32975401a098f096a3b1f14ff3d8f
Victor Groux <victorgroux99@gmail.com>
2026-06-08 09:13:56 +0200

    Free is now more ok   It cache at least one page for TINY and SMALL zone

--- srcs/free.c
+++ srcs/free.c
@@ -2,6 +2,25 @@
 
 extern t_malloc_state	gMallocState;
 
+static	void	free_page(t_heap *heap)
+{
+	if (heap->prev != NULL)
+		heap->prev->next = heap->next;
+	if (heap->next != NULL)
+		heap->next->prev = heap->prev;
+
+	if (heap->group == TINY && gMallocState.tiny == heap)
+		gMallocState.tiny = heap->next;
+	else if (heap->group == SMALL && gMallocState.small == heap)
+		gMallocState.small = heap->next;
+	else if (heap->group == LARGE && gMallocState.large == heap)
+		gMallocState.large = heap->next;
+
+	heap->prev = NULL;
+	heap->next = NULL;
+	munmap(heap, heap->total_size);
+}
+
 void	free(void *ptr)
 {
 	if (ptr == NULL)
@@ -9,22 +28,26 @@ void	free(void *ptr)
 
 	t_block	*block = (t_block *)((char *)ptr - sizeof(t_block));
 	t_heap	*heap = retrieve_heap(block);
-	if (heap == NULL)
+	if (heap == NULL || block->in_use == 0)
 		return;
 
 	block->in_use = 0;
 	heap->free_size += block->size;
 	coalescence(heap, block);
 
-	if (heap->block_count == 1 && heap->blocks->in_use == 0)
+	if (heap->block_count == 1 && heap->free_size == heap->total_size - sizeof(t_heap) - sizeof(t_block))
 	{
+		if (heap->group == LARGE)
+		{
+			free_page(heap);
+			return;
+		}
+
 		t_heap**	head = NULL;
 		if (heap->group == TINY)
 			head = &gMallocState.tiny;
-		else if (heap->group == SMALL)
-			head = &gMallocState.small;
 		else
-			head = &gMallocState.large;
+			head = &gMallocState.small;
 
 		t_heap*	cursor = *head;
 		int		empty = 0;
@@ -36,20 +59,6 @@ void	free(void *ptr)
 		}
 
 		if (empty != 0)
-		{
-			if (heap->prev != NULL)
-				heap->prev->next = heap->next;
-			if (heap->next != NULL)
-				heap->next->prev = heap->prev;
-
-			if (heap->group == TINY && gMallocState.tiny == heap)
-				gMallocState.tiny = heap->next;
-			else if (heap->group == SMALL && gMallocState.small == heap)
-				gMallocState.small = heap->next;
-			else if (heap->group == LARGE && gMallocState.large == heap)
-				gMallocState.large = heap->next;
-
-			munmap(heap, heap->total_size);
-		}
+			free_page(heap);
 	}
 }
